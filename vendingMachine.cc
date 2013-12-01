4a2db4f743e40103662c29f0de10065daa8a97f3
RylanHalteman <rylan.halteman@gmail.com>
2013-11-30 21:13:12 -0500

    touched up vendingMachine

--- vendingMachine.cc
+++ vendingMachine.cc
@@ -1,26 +1,41 @@
 #include "vendingMachine.h"
+#include "watcard.h"
 #include "nameServer.h"
 
 VendingMachine::VendingMachine( Printer &prt, NameServer &nameServer, unsigned int id, unsigned int sodaCost,
-                    unsigned int maxStockPerFlavour ) : prt(&prt), id(id) {
+                    unsigned int maxStockPerFlavour ) : prt(&prt), id(id), sodaCost(sodaCost), 
+                    maxStock(maxStockPerFlavour) {
 	nameServer.VMregister( this );
+	for (int i = 0; i < 4; i += 1) {
+		sodaInventory[i] = 0;
+	}
 }
 
 
 void
 VendingMachine::main() {
-
+	for(;;) {
+		_Accept( inventory ) {
+			_Accept( restocked );
+		}
+		or _Accept( buy );
+		or _Accept( ~VendingMachine ) { break; }
+	}
 }
 
 
 VendingMachine::Status
 VendingMachine::buy( Flavours flavour, WATCard &card ) {
+	if ( sodaInventory[flavour] == 0 ) { return STOCK; }
+	if ( card.getBalance() < sodaCost )	{ return FUNDS; }
 
+	card.withdraw(sodaCost);
+	return BUY;	
 }
 
 unsigned int *
 VendingMachine::inventory() {
-	return 0;
+	return sodaInventory;
 }
 
 void
@@ -30,7 +45,7 @@ VendingMachine::restocked() {
 
 unsigned int
 VendingMachine::cost() {
-	return 0;
+	return sodaCost;
 }
 
 unsigned int
--- vendingMachine.h
+++ vendingMachine.h
@@ -9,14 +9,18 @@ _Task NameServer;
 _Task VendingMachine {
     Printer *prt;
     unsigned int id;
+    unsigned int sodaCost;
+    unsigned int maxStock;
+    unsigned int sodaInventory[4];
+
     void main();
   public:
-    enum Flavours {  };                 // flavours of soda (YOU DEFINE)
+    enum Flavours { BLACK_CHERRY, CREAM_SODA, ROOT_BEER, LIME };                 // flavours of soda (YOU DEFINE)
     enum Status { BUY, STOCK, FUNDS };     // purchase status: successful buy, out of stock, insufficient funds
     VendingMachine( Printer &prt, NameServer &nameServer, unsigned int id, unsigned int sodaCost,
                     unsigned int maxStockPerFlavour );
     Status buy( Flavours flavour, WATCard &card );
-    unsigned int *inventory();
+    unsigned int* inventory();
     void restocked();
     _Nomutex unsigned int cost();
     _Nomutex unsigned int getId();
